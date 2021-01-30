4e84db197a1f196429f1b21d062da7be527ccb97
Ryan <rwatson1799@gmail.com>
2021-01-30 12:14:53 +1000

    Added the time bar

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -78,6 +78,18 @@ int main()
 	// Variables to control time
 	Clock clock;
 
+	// Time bar
+	RectangleShape timeBar;
+	float timeBarStartWidth = 400;
+	float timeBarHeight = 80;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
 	// Track whether the game is running
 	bool paused = true;
 
@@ -136,6 +148,10 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Enter))
 		{
 			paused = false;
+
+			// Reset the time and the score
+			score = 0;
+			timeRemaining = 6;
 		}
 
 		/*
@@ -148,6 +164,25 @@ int main()
 		{
 			// Measure time
 			Time dt = clock.restart();
+
+			// Subtract from the amount of time remaining
+			timeRemaining -= dt.asSeconds();
+
+			// resize up the time bar
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
+
+			if (timeRemaining <= 0.0f) {
+				// Paus the game
+				paused = true;
+
+				// Change the message shown to the player
+				messageText.setString("Out of time!!");
+
+				// Reposition the text based on its new size
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+				messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+			}
 
 			// Setup the bee
 			if (!beeActive)
@@ -291,6 +326,9 @@ int main()
 
 		// Draw the score
 		window.draw(scoreText);
+
+		// Draw the timebar
+		window.draw(timeBar);
 
 		if (paused)
 		{
